f3192ad1fcf2cd0c2e161f10d68a51b0be281b9b
hoshi524 <hoshi524@gmail.com>
2016-10-29 08:35:56 +0900

    update

--- comple.sh
+++ comple.sh
@@ -1,3 +1,5 @@
 
 cd `dirname $0`
-g++ -std=c++11 -O2 Main.cpp
+g++ -std=c++11 -O2 -o b.out Main.cpp
+g++ -std=c++11 -O2 -o a.out Test.cpp
+
--- time.sh
+++ time.sh
@@ -4,12 +4,12 @@ cd `dirname $0`
 for file in `\find input -maxdepth 1 -type f`; do
   echo $file
 
-  echo $1
-  time $1 < $file > /dev/null
-  echo $2
-  time $2 < $file > /dev/null
-  echo $1
-  time $1 < $file > /dev/null
-  echo $2
-  time $2 < $file > /dev/null
+  echo a
+  time ./a.out < $file > /dev/null
+  echo b
+  time ./b.out < $file > /dev/null
+  echo a
+  time ./a.out < $file > /dev/null
+  echo b
+  time ./b.out < $file > /dev/null
 done
